79dd3cdf7918baadf7e62e984c490e6e1fa521b9
William Koh <wkoh@hmc.edu>
2024-04-22 18:11:34 -0700

    Update data_smoothing.py

--- python_files/data_smoothing.py
+++ python_files/data_smoothing.py
@@ -1,4 +1,5 @@
-import InformationSchemaParser
+from InformationSchemaParser import DatabaseDDLGenerator
+from ollama import Client
 import json
 import ollama
 
@@ -8,10 +9,12 @@ database_name = "tutos"
 host = "localhost"
 user = "tuto"
 password = "admingres"
-ddl_generator = InformationSchemaParser.DatabaseDDLGenerator(database_name, host, user, password)
+ddl_generator = DatabaseDDLGenerator(database_name, host, user, password)
+
+import pdb;
 
 # input strings for the 
-question = "Which product generates the most sales?"
+question = "How many movies in each genre were released per year?"
 
 input_instructions = f"""
 ### Instructions:
@@ -27,15 +30,27 @@ input_init_message = f"""
 Generate a SQL query that answers the question `{question}`.
 This query will run on a database whose schema is represented in this string:"""
 
-input_script_message = ddl_generator.print_marshalled_ddl_statements()
+input_script_message = ddl_generator.ddl_statements_prompt()
 
 input_response = f"""
 ### Response:
 Based on your instructions, here is the SQL query I have generated to answer the question `{question}`:
 ```sql
 """
+#comment =  """COMMENT ON COLUMN "movies"."release_year" IS 'The small integer YYYY labeling the year of the movie release';"""
+input_entire_message = input_instructions + '\n' + '\n' + input_init_message + '\n' + input_script_message + '\n'+ '\n'+  '\n' + input_response
 
-input_entire_message = input_instructions + '\n' + '\n' + input_init_message + '\n' + input_script_message + '\n'+ '\n'+ input_response
+# client = Client(host='http://localhost:11434')
+# print(input_entire_message)
+# response = client.chat(model='sqlcoder', messages=[
+#   {
+#     'role': 'user',
+#     'content': input_entire_message,
+#   },
+# ])
+
+# print(response['message']['content'])
+
 
 stream = ollama.chat(
     model='sqlcoder',
